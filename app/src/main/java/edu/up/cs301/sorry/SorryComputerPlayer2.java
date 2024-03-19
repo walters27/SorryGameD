fa2618480e035c884b1fb32d0a2f49b5bf33bc17
anneliese walters <walters27@up.edu>
2024-03-19 09:19:25 -0700

    03/19/24

--- app/src/main/java/edu/up/cs301/sorry/SorryComputerPlayer2.java
+++ app/src/main/java/edu/up/cs301/sorry/SorryComputerPlayer2.java
@@ -90,7 +90,7 @@ public class SorryComputerPlayer2 extends SorryComputerPlayer1 {
 					new Runnable() {
 						public void run() {
 						if (counterValueTextView != null && currentGameState != null) {
-							counterValueTextView.setText("" + currentGameState.getCounter());
+							counterValueTextView.setText("" + currentGameState.getClass());
 						}
 					}});
 		}
